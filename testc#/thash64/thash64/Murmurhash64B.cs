a26ec8705eea5c9f890aa3e88840f1aa9b7d2b11
wanwei <uniqs@163.com>
2023-10-27 15:38:01 +0800

    c# test ok.

---
+++ testc#/thash64/thash64/Murmurhash64B.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+namespace utility
+{
+    public class MurmurHash64B
+    {
+        public static ulong MakeHashValue(byte[] key, uint seed = 0xee6b27eb)
+        {
+            uint len = (uint)key.Length;
+            const uint m = 0x5bd1e995;
+            const int r = 24;
+ 
+            uint h1 = seed ^ len;
+            uint h2 = 0;
+            int pos = 0;
+ 
+            while (len >= 8)
+            {
+                uint k1 = System.BitConverter.ToUInt32(key, pos);
+                pos += 4;
+                k1 *= m; k1 ^= k1 >> r; k1 *= m;
+                h1 *= m; h1 ^= k1;
+                len -= 4;
+ 
+                uint k2 = System.BitConverter.ToUInt32(key, pos);
+                pos += 4;
+                k2 *= m; k2 ^= k2 >> r; k2 *= m;
+                h2 *= m; h2 ^= k2;
+                len -= 4;
+            }
+ 
+            if (len >= 4)
+            {
+                uint k1 = System.BitConverter.ToUInt32(key, pos);
+                pos += 4;
+                k1 *= m; k1 ^= k1 >> r; k1 *= m;
+                h1 *= m; h1 ^= k1;
+                len -= 4;
+            }
+ 
+            if (len == 3)
+            {
+                h2 ^= (uint)key[pos + 2] << 16;
+                h2 ^= (uint)key[pos + 1] << 8;
+                h2 ^= (uint)key[pos + 0];
+                h2 *= m;
+            }
+            else if (len == 2)
+            {
+                h2 ^= (uint)key[pos + 1] << 8;
+                h2 ^= (uint)key[pos + 0];
+                h2 *= m;
+            }
+            else if (len == 1)
+            {
+                h2 ^= (uint)key[pos + 0];
+                h2 *= m;
+            }
+ 
+            h1 ^= h2 >> 18; h1 *= m;
+            h2 ^= h1 >> 22; h2 *= m;
+            h1 ^= h2 >> 17; h1 *= m;
+            h2 ^= h1 >> 19; h2 *= m;
+ 
+            ulong h = h1;
+ 
+            h = (h << 32) | h2;
+ 
+            return h;
+        }
+ 
+        public static ulong StringToHashValue(string source)
+        {
+            byte[] bytes = System.Text.Encoding.Default.GetBytes(source);
+            return MakeHashValue(bytes);
+        }
+    }
+}
+
---
+++ testc#/thash64/thash64/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("c# is shit");
+
+
+void test(string str, ulong expect)
+{
+    ulong ret = utility.MurmurHash64B.MakeHashValue(Encoding.UTF8.GetBytes(str));
+    if (expect == ret)
+    {
+        Console.WriteLine("ok.");
+    } else
+    {
+        Console.WriteLine();
+        Console.WriteLine("not ok.");
+        Console.WriteLine(expect);
+        Console.WriteLine(ret);
+        Console.WriteLine(str);
+    }
+}
+void Main()
+{
+    test("hello world", 15872188016033428291ul);
+    test("hello|world", 18154384023894766483ul);
+    test("world hello", 3885965473734762678ul);
+    test("123456789|1001", 3735326771242004321ul);
+    test("123456789|1002", 1645870013858767195ul);
+    Console.WriteLine("");
+}
+
+Main();
