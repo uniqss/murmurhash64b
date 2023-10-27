d7c8504ac35e77ce36a9a4ba2060362b8475290d
wanwei <uniqs@163.com>
2023-10-27 18:13:51 +0800

    ok

--- publish.bat
+++ publish.bat
@@ -1,3 +1,4 @@
+copy .\testscpp\OtherAlgorithms\murmurhash64b.hpp .\cpp\
 copy .\testsgo\murmurhash64b.go .\go\
 copy .\testc#\thash64\thash64\Murmurhash64B.cs .\c#\
 
--- testscpp/testmurmurhash64b.cpp
+++ testscpp/testmurmurhash64b.cpp
@@ -1,4 +1,4 @@
-#include "../c++/murmurhash64b.hpp"
+#include "./OtherAlgorithms/murmurhash64b.hpp"
 
 #include <string>
 #include <iostream>
@@ -7,13 +7,23 @@ using std::cout;
 using std::endl;
 
 void test(const string& str, unsigned long long expect) {
-    auto ret = utility::MurmurHash64B(str.c_str(), (int)str.size());
+    auto ret = utility::Murmurhash64bStr(str);
     if (expect == ret) {
         cout << "ok." << endl;
     } else {
         cout << "not ok." << endl;
         cout << "str:" << str << endl;
         cout << "expected: " << expect << " ret: " << ret << endl;
+    }
+}
+void testii(int64_t i1, int i2, unsigned long long expect) {
+    auto ret = utility::Murmurhash64bII(i1, i2);
+    if (expect == ret) {
+        cout << "ok." << endl;
+    } else {
+        cout << "not ok." << endl;
+        cout << "expect:" << expect << " ret:" << ret << endl;
+        cout << "i1:" << i1 << " i2:" << i2 << endl;
     }
 }
 
@@ -23,5 +33,8 @@ int main() {
     test("world hello", 3885965473734762678ul);
     test("123456789|1001", 3735326771242004321ul);
     test("123456789|1002", 1645870013858767195ul);
+    test("你好世界", 3269873437238233672ul);
+    test("1717720317549350912|1001", 9344049702176278212ul);
+    testii(1717720317549350912, 1001, 3168977379835420589ul);
     return 0;
 }
